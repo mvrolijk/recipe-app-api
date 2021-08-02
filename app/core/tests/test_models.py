a1456d378561eed2bba3e22f8cfbf5cc02924a9d
Miguel Vrolijk <miguel.vrolijk@gmail.com>
2021-08-02 15:08:17 +0300

    All my work up to now

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,7 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
         
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@MGV.DYNU.NET'
+        
